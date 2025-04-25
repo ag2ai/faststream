bb63b54c69cf9469d917aa6bcd3530c3c2eaf0ec
0xwebmilk <0xwebmilk@gmail.com>
2025-04-25 21:06:56 +0300

    Refactored

--- faststream/exceptions.py
+++ faststream/exceptions.py
@@ -130,21 +130,26 @@ pip install watchfiles
 """
 
 INSTALL_FASTSTREAM_RABBIT = """
-To use RabbitMQ with fastream, please install dependencies:\n
+To use RabbitMQ with faststream, please install dependencies:\n
 pip install faststream[rabbit]
 """
 
 INSTALL_FASTSTREAM_KAFKA = """
-To use Apache Kafka with fastream, please install dependencies:\n
+To use Apache Kafka with faststream, please install dependencies:\n
 pip install faststream[kafka]
 """
 
+INSTALL_CONFLUENT_KAFKA = """
+To use Confluent Kafka with faststream, please install dependencies:\n
+pip install faststream[confluent]"""
+
+
 INSTALL_FASTSTREAM_REDIS = """
-To use Redis with fastream, please install dependencies:\n
+To use Redis with faststream, please install dependencies:\n
 pip install faststream[redis]
 """
 
 INSTALL_FASTSTREAM_NATS = """
-To use NATS with fastream, please install dependencies:\n
+To use NATS with faststream, please install dependencies:\n
 pip install faststream[nats]
 """
