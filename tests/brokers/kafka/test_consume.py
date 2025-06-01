1a382f6d8cdc547e9e0dcbe48d1e6565b30dbdf9
Nikita Pastukhov <diementros@yandex.ru>
2025-06-01 19:10:07 +0300

    fix: correct empty task initialization

--- tests/brokers/kafka/test_consume.py
+++ tests/brokers/kafka/test_consume.py
@@ -554,9 +554,9 @@ class TestConsume(KafkaTestcaseConfig, BrokerRealConsumeTestcase):
     ),
 )
 async def test_concurrent_consume_between_partitions_assignment_warning(
-    queue: str,
     partitions: int,
     warning: bool,
+    queue: str,
     monkeypatch: pytest.MonkeyPatch,
 ) -> None:
     monkeypatch.setattr(_LoggingListener, "_log_unassigned_consumer_delay_seconds", 0)
@@ -580,26 +580,24 @@ async def test_concurrent_consume_between_partitions_assignment_warning(
     async def handler(msg: str) -> None:
         pass
 
-    # with patch.object(consume_broker, "_state", MagicMock(handlers=[])) as mock:
     async with consume_broker as broker:
         await broker.start()
-        await broker.close()
 
-        if warning:
-            assert (
-                len([
-                    x for x in logger.log.call_args_list if x[0][0] == logging.WARNING
-                ])
-                == 2
-            )
+    if warning:
+        assert (
+            len([
+                x for x in logger.log.call_args_list if x[0][0] == logging.WARNING
+            ])
+            == 2
+        )
 
-        else:
-            assert (
-                len([
-                    x for x in logger.log.call_args_list if x[0][0] == logging.WARNING
-                ])
-                == 0
-            )
+    else:
+        assert (
+            len([
+                x for x in logger.log.call_args_list if x[0][0] == logging.WARNING
+            ])
+            == 0
+    )
 
 
 @pytest.mark.asyncio()
