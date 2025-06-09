fd0c3b1390be9403721551dba03c82d857608d0f
Nikita Zharov <nikitazharov2@mail.ru>
2025-06-09 23:18:42 +0300

    fix(depends): removed SetupError if correct fastapi.Depends is used

--- tests/brokers/base/fastapi.py
+++ tests/brokers/base/fastapi.py
@@ -171,6 +171,35 @@ class FastAPITestcase(BaseTestcaseConfig):
         with pytest.raises(SetupError), TestClient(app):
             ...
 
+    async def test_combined_context_annotated(
+        self, mock: Mock, queue: str, event: asyncio.Event
+    ):
+        router = self.router_class()
+
+        context_key = "message.headers"
+
+        args, kwargs = self.get_subscriber_params(queue)
+
+        @router.subscriber(*args, **kwargs)
+        async def hello(
+            msg: Annotated[Any, Context(context_key), FSContext(context_key)],
+        ):
+            event.set()
+            return mock(msg == context.resolve(context_key))
+
+        async with router.broker:
+            await router.broker.start()
+            await asyncio.wait(
+                (
+                    asyncio.create_task(router.broker.publish("", queue)),
+                    asyncio.create_task(event.wait()),
+                ),
+                timeout=self.timeout,
+            )
+
+        assert event.is_set()
+        mock.assert_called_with(True)
+
     async def test_initial_context(self, queue: str, event: asyncio.Event):
         router = self.router_class()
 
@@ -463,6 +492,30 @@ class FastAPILocalTestcase(BaseTestcaseConfig):
 
         with pytest.raises(SetupError), TestClient(app):
             ...
+
+    async def test_depends_combined_annotated(self, mock: Mock, queue: str):
+        router = self.router_class()
+
+        def dep(a):
+            mock(a)
+            return a
+
+        args, kwargs = self.get_subscriber_params(queue)
+
+        @router.subscriber(*args, **kwargs)
+        async def hello(a, w: Annotated[Any, FSDepends(dep), Depends(dep)]):
+            return w
+
+        async with self.broker_test(router.broker):
+            r = await router.broker.publish(
+                {"a": "hi"},
+                queue,
+                rpc=True,
+                rpc_timeout=0.5,
+            )
+            assert r == "hi"
+
+        mock.assert_called_once_with("hi")
 
     async def test_yield_depends(self, mock: Mock, queue: str):
         router = self.router_class()
