f2bc1c673cc2d0bff9e8899e6821fabef6acd916
computerScienceEnjoyer <97458127+C3EQUALZz@users.noreply.github.com>
2025-07-01 01:00:04 +0300

    Update registrator.py
    fixed a typo related to the class name for an incoming message from Redis

--- faststream/redis/broker/registrator.py
+++ faststream/redis/broker/registrator.py
@@ -56,7 +56,7 @@ class RedisRegistrator(ABCBroker[UnifyRedisDict]):
         parser: Annotated[
             Optional["CustomCallable"],
             Doc(
-                "Parser to map original **aio_pika.IncomingMessage** Msg to FastStream one."
+                "Parser to map original **redis.ResponseT (Union[Awaitable[Any], Any])** Msg to FastStream one."
             ),
         ] = None,
         decoder: Annotated[
