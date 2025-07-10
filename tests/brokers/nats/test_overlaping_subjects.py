aeb21a9d4e4822a7dc2485ae826208c991485952
GADEEV Evgeny <Evgeny.GADEEV@raiffeisen.ru>
2025-07-10 16:42:23 +0300

    fix test

--- tests/brokers/nats/test_overlaping_subjects.py
+++ tests/brokers/nats/test_overlaping_subjects.py
@@ -1,5 +1,8 @@
 import pytest
 
+from faststream.nats.broker.broker import filter_overlapped_subjects
+
+
 @pytest.mark.parametrize(
     ("subjects", "expected"),
     [
@@ -10,8 +13,14 @@ import pytest
         (["a.b", "b.b"], {"a.b", "b.b"}),
         (["a.b", "a.*"], {"a.*"}),
         (["a.b.c", "a.*.c"], {"a.*.c"}),
-        (["*.b.c", "a.>", "a.b.c"], {"*.b.c", "a.>"}),  # <- this case will raise subject "a.>" overlaps with "*.b.c"
-        (["a.b", "a.*", "a.b.c"], {"a.*", "a.b.c"}),  # but I don't know what to do with it
+        (
+            ["*.b.c", "a.>", "a.b.c"],
+            {"*.b.c", "a.>"},
+        ),  # <- this case will raise subject "a.>" overlaps with "*.b.c"
+        (
+            ["a.b", "a.*", "a.b.c"],
+            {"a.*", "a.b.c"},
+        ),  # but I don't know what to do with it
         (["a.b", "a.>", "a.b.c"], {"a.>"}),
         (["a.*", "a.>"], {"a.>", "a.*"}),
         (["a.*", "a.>", "a.b"], {"a.>", "a.*"}),
