1acea39f9689b0d4a949c8a5c593c0639a77b3b0
Eugene Gadeev <xgemx@rambler.ru>
2025-07-10 16:30:01 +0300

    filter overlapped subjects

---
+++ tests/brokers/nats/test_overlaping_subjects.py
@@ -0,0 +1,25 @@
+import pytest
+
+@pytest.mark.parametrize(
+    ("subjects", "expected"),
+    [
+        (["a"], {"a"}),
+        (["a", "a"], {"a"}),
+        (["a.b", "a.b"], {"a.b"}),
+        (["a", "b"], {"a", "b"}),
+        (["a.b", "b.b"], {"a.b", "b.b"}),
+        (["a.b", "a.*"], {"a.*"}),
+        (["a.b.c", "a.*.c"], {"a.*.c"}),
+        (["*.b.c", "a.>", "a.b.c"], {"*.b.c", "a.>"}),  # <- this case will raise subject "a.>" overlaps with "*.b.c"
+        (["a.b", "a.*", "a.b.c"], {"a.*", "a.b.c"}),  # but I don't know what to do with it
+        (["a.b", "a.>", "a.b.c"], {"a.>"}),
+        (["a.*", "a.>"], {"a.>", "a.*"}),
+        (["a.*", "a.>", "a.b"], {"a.>", "a.*"}),
+        (["a.*.*", "a.>"], {"a.>"}),
+        (["a.*.*", "a.*.*.*", "a.b.c", "a.b.c.d", "a.>"], {"a.>"}),
+        (["a.>", "a.*.*", "a.*.*.*", "a.b.c", "a.b.c.d"], {"a.>"}),
+        (["a.*.*", "a.*.*.*", "a.b.c", "a.b.c.d"], {"a.*.*", "a.*.*.*"}),
+    ],
+)
+def test_filter_overlapped_subjects(subjects: list[str], expected: set[str]) -> None:
+    assert set(filter_overlapped_subjects(subjects)) == expected
