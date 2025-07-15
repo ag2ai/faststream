f07cb2cfff13995ea518b0f392811af461d18edd
kodsurfer <kodsurfer@pm.me>
2025-07-15 19:35:29 +0300

    #2365 add test_router_publisher_prefix

--- tests/brokers/base/router.py
+++ tests/brokers/base/router.py
@@ -119,6 +119,40 @@ class RouterTestcase(
 
             assert event.is_set()
 
+    async def test_router_publisher_prefix(
+        self,
+        router: BrokerRouter,
+        pub_broker: BrokerUsecase,
+        queue: str,
+        event: asyncio.Event,
+    ):
+        response_topic = f"{queue}_response"
+        publisher = router.publisher(response_topic)
+
+        @router.subscriber(queue)
+        @publisher
+        async def handler(m):
+            return "response"
+
+        @pub_broker.subscriber(f"test_{response_topic}")
+        def response_handler(m):
+            event.set()
+
+        pub_broker.include_router(router, prefix="test_")
+
+        async with pub_broker:
+            await pub_broker.start()
+
+            await asyncio.wait(
+                (
+                    asyncio.create_task(pub_broker.publish("hello", f"test_{queue}")),
+                    asyncio.create_task(event.wait()),
+                ),
+                timeout=self.timeout,
+            )
+
+            assert event.is_set()
+
     async def test_empty_prefix_publisher(
         self,
         router: BrokerRouter,
