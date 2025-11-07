86e6a3320d901ba542f6b04e2a2d867943179bcc
sheldy <chikibamtioma@gmail.com>
2025-11-07 01:55:05 +0100

    move Schedule to dataclass

--- faststream/nats/schemas/schedule.py
+++ faststream/nats/schemas/schedule.py
@@ -1,14 +1,10 @@
+from dataclasses import dataclass
 from datetime import datetime
 
-from faststream._internal.proto import NameRequired
 
-
-class Schedule(NameRequired):
+@dataclass
+class Schedule:
     """A class to represent a message schedule."""
 
-    __slots__ = ("target", "time")
-
-    def __init__(self, time: datetime | str, target: str, /) -> None:
-        super().__init__(target)
-        self.time = datetime.fromisoformat(time) if isinstance(time, str) else time
-        self.target = target
+    time: datetime
+    target: str
