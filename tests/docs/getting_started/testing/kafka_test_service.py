d9d770965ca366fce94a062540993dedfaadc44a
literally user <literally_user@hotmail.com>
2026-01-08 16:40:55 +0300

    add: missing tests to documentation

---
+++ tests/docs/getting_started/testing/kafka_test_service.py
@@ -0,0 +1,21 @@
+import pytest
+from pydantic import ValidationError
+
+from faststream import FastStream
+from faststream.kafka import KafkaBroker, TestKafkaBroker
+
+broker = KafkaBroker("localhost:9092")
+app = FastStream(broker)
+
+
+@pytest.mark.asyncio()
+async def test_correct() -> None:
+    async with TestKafkaBroker(broker) as br:
+        await br.publish(...)
+
+
+@pytest.mark.asyncio()
+async def test_invalid() -> None:
+    async with TestKafkaBroker(broker) as br:  # noqa: F841
+        with pytest.raises(ValidationError):
+            ...
