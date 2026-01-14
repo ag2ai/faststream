c2a9954aae81cbf661cba6a2afefdf4dc6c8ee5f
Flosckow <danildumchenko13@gmail.com>
2026-01-15 03:00:18 +0700

    Remove

--- faststream/_internal/fastapi/_compat.py
+++ faststream/_internal/fastapi/_compat.py
@@ -57,13 +57,6 @@ if FASTAPI_V128:
     def raise_fastapi_validation_error(errors: list[Any], body: dict[str, Any]) -> Never:
         raise RequestValidationError(errors, body=body)
 
-# elif FASTAPI_V2:
-#     from fastapi._compat import _normalize_errors
-#     from fastapi.exceptions import RequestValidationError
-
-#     def raise_fastapi_validation_error(errors: list[Any], body: dict[str, Any]) -> Never:
-#         raise RequestValidationError(_normalize_errors(errors), body=body)
-
 else:
     from pydantic import (  # type: ignore[assignment]
         ValidationError as RequestValidationError,
