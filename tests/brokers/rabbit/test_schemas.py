f1bacfa9b5a4da38a17772ba1f9c8420436191e2
RinZ27 <222222878+RinZ27@users.noreply.github.com>
2026-03-05 09:58:02 +0700

    fix: consistent hashing and equality for RabbitMQ schemas

--- tests/brokers/rabbit/test_schemas.py
+++ tests/brokers/rabbit/test_schemas.py
@@ -1,6 +1,6 @@
 import pytest
 
-from faststream.rabbit import RabbitQueue
+from faststream.rabbit import Channel, RabbitExchange, RabbitQueue
 
 
 @pytest.mark.rabbit()
@@ -22,4 +22,51 @@ def test_different_queue_routing_key() -> None:
             RabbitQueue("test", routing_key="binding-2"): 1,
         })
         == 1
+    )
+
+
+@pytest.mark.rabbit()
+def test_different_queue_params() -> None:
+    assert (
+        len({
+            RabbitQueue("test", durable=True): 0,
+            RabbitQueue("test", durable=False): 1,
+        })
+        == 2
+    )
+
+
+@pytest.mark.rabbit()
+def test_exchange_equality() -> None:
+    assert (
+        len({
+            RabbitExchange("test", durable=True): 0,
+            RabbitExchange("test", durable=True): 1,
+        })
+        == 1
+    )
+    assert (
+        len({
+            RabbitExchange("test", durable=True): 0,
+            RabbitExchange("test", durable=False): 1,
+        })
+        == 2
+    )
+
+
+@pytest.mark.rabbit()
+def test_channel_equality() -> None:
+    assert (
+        len({
+            Channel(prefetch_count=10): 0,
+            Channel(prefetch_count=10): 1,
+        })
+        == 1
+    )
+    assert (
+        len({
+            Channel(prefetch_count=10): 0,
+            Channel(prefetch_count=20): 1,
+        })
+        == 2
     )
