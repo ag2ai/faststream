7e76c4a759d286459c84d8508bb7f624a3a3379c
Arseniy Popov <arseniypopov@gmail.com>
2026-04-04 00:26:20 +0300

    feat: add sqla broker

--- tests/brokers/sqla/test_misconfigure.py
+++ tests/brokers/sqla/test_misconfigure.py
@@ -1,6 +1,6 @@
 import pytest
 import pytest_asyncio
-from sqlalchemy.ext.asyncio import AsyncEngine
+from sqlalchemy.ext.asyncio import AsyncEngine, create_async_engine
 
 from faststream import AckPolicy
 from faststream.sqla import SqlaBroker
@@ -9,7 +9,7 @@ from faststream.sqla.retry import NoRetryStrategy
 
 @pytest_asyncio.fixture
 async def broker(engine: AsyncEngine) -> SqlaBroker:
-    return SqlaBroker(engine=engine)
+    return SqlaBroker(engine=create_async_engine("sqlite+aiosqlite:///:memory:"))
 
 
 @pytest.mark.sqla()
