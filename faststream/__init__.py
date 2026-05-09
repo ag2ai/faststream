8f2817172a4d09906d482bccaae7b979198d65ad
SarthakB11 <sarthak.bhardwaj21b@iiitg.ac.in>
2026-05-09 18:51:08 +0000

    feat: re-export BrokerUsecase from top-level package
    Fixes #2806

--- faststream/__init__.py
+++ faststream/__init__.py
@@ -1,5 +1,6 @@
 """A Python framework for building services interacting with Apache Kafka, RabbitMQ, NATS and Redis."""
 
+from faststream._internal.broker.broker import BrokerUsecase
 from faststream._internal.testing.app import TestApp
 from faststream._internal.utils import apply_types
 from faststream.annotations import ContextRepo, Logger
@@ -15,6 +16,7 @@ __all__ = (
     "AsyncAPI",
     "BaseMiddleware",
     "BatchPublishCommand",
+    "BrokerUsecase",
     "Context",
     "ContextRepo",
     "Depends",
