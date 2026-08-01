0127a5435759f6bbafc20bae2fa8387c37026518
Loi Nguyen <vanloi999@gmail.com>
2026-08-01 21:53:23 +0700

    test(cli): strip ANSI escapes in startup error assertion

--- tests/cli/utils/test_errors.py
+++ tests/cli/utils/test_errors.py
@@ -1,5 +1,9 @@
+import re
+
 from faststream._internal.cli.utils.errors import draw_startup_errors
 from faststream.exceptions import StartupValidationError
+
+ANSI_ESCAPE_RE = re.compile(r"\x1b\[[0-9;]*m")
 
 
 def test_draw_startup_errors(capsys) -> None:
@@ -10,6 +14,6 @@ def test_draw_startup_errors(capsys) -> None:
         ),
     )
 
-    stderr = capsys.readouterr().err
+    stderr = ANSI_ESCAPE_RE.sub("", capsys.readouterr().err)
     assert "--wrong" in stderr
     assert "--required" in stderr
