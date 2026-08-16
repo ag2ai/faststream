adcd28d57f02c033075c2265bd24edc58292ce0d
borisalekseev <i.borisalekseev@gmail.com>
2026-08-16 15:59:56 +0300

    feat: Support MQTT Will message

--- tests/mypy/mqtt.py
+++ tests/mypy/mqtt.py
@@ -1,8 +1,18 @@
-from faststream.mqtt import MQTTBroker, TestMQTTBroker
+from faststream.mqtt import MQTTBroker, QoS, TestMQTTBroker, Will, WillProperties
 
 MQTTBroker("mqtts://localhost")
 MQTTBroker(url="mqtt://localhost:1884")
 MQTTBroker(host="localhost", port=1884)
+MQTTBroker(
+    version="5.0",
+    will=Will(
+        topic="status/service",
+        payload=b"offline",
+        qos=QoS.AT_LEAST_ONCE,
+        retain=True,
+        properties=WillProperties(content_type="text/plain"),
+    ),
+)
 
 
 async def check_multiple_test_brokers() -> None:
