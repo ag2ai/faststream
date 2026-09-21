96f8862f5589de0c6e6836aff535ff937a97b73d
Danil Kovalev <97458127+C3EQUALZz@users.noreply.github.com>
2026-09-21 10:20:02 +0300

    Update faststream/nats/security.py
    Co-authored-by: Ivan Kirpichnikov <mmssvvvv570@gmail.com>

--- faststream/nats/security.py
+++ faststream/nats/security.py
@@ -321,15 +321,14 @@ class NatsNKey(NatsSecurity):
             msg = "NATS NKey seed cannot be empty."
             raise ValueError(msg)
 
-        security = cls(
+        return cls(
             seed,
             ssl_context=ssl_context,
             use_ssl=use_ssl,
             tls_hostname=tls_hostname,
             tls_handshake_first=tls_handshake_first,
+            seed_is_file=False,
         )
-        security.seed_is_file = False
-        return security
 
 
 class NatsJWT(NatsSecurity):
