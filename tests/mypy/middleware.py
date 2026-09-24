0f6b91cb83620574e5505a92d2339fe7c0b72a8f
Apus Berliozi <romanrabota602@gmail.com>
2026-09-25 03:38:13 +0700

    chore: [#2070] added new type var for messages, added tests in tests/mypy

--- tests/mypy/middleware.py
+++ tests/mypy/middleware.py
@@ -44,4 +44,4 @@ def check_plain_init(msg: RawMessage, context: ContextRepo) -> None:
 
 def check_typed_init(msg: RawMessage, context: ContextRepo) -> None:
     assert_type(Typed(msg, context=context).msg, RawMessage)
-    Typed(None, context=context)  # type: ignore[arg-type]
+    
